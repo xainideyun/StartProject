79245f7b0d13d80c5eff30b266b78d43690785c4
sunxsh <sunxsaljj@163.com>
2016-05-09 08:25:51 +0800

    modify

---
+++ wcf/WcfService/WcfConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLib;
+
+namespace WcfConsole
+{
+    // 把Endpoint中的ABC，基地址，Behaviors等直接写在代码里，不依赖配置文件
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:9999/User");                    // 创建宿主的基地址
+            using (ServiceHost host = new ServiceHost(typeof(User), baseAddress))
+            {
+                host.AddServiceEndpoint(typeof(IUser), new WSHttpBinding(), "");        // 向宿主中添加终结点
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();            // 将HttpGetEnabled数据设置为true
+                smb.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(smb);                                    // 将行为添加到Behaviors中
+                host.Open();                                                            // 打开宿主
+                Console.WriteLine("开始监听9999端口");
+                Console.Read();
+                host.Close();
+            }
+        }
+    }
+}
---
+++ wcf/WcfService/WcfPractice/WcfFirst.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WcfFirst.aspx.cs" Inherits="WcfPractice.WcfFirst" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnSubmit" runat="server" Text="确定" OnClick="btnSubmit_Click" />
+        <asp:Label ID="lblName" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
