9978385881b12148c9247f0c4926497a1c55a10a
sunxsh <sunxsaljj@163.com>
2016-07-04 14:22:26 +0800

    不知道改了什么，提交了吧！

---
+++ nodejs/http/get.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var url = require('url');
+http.createServer(function (req,res) {
+  res.writeHead(200,{'Content-Type':'text/plain;charset=utf-8;'})
+  res.write(require('util').inspect(url.parse(req.url,true)));
+  res.end();
+}).listen(5000);
+console.log('端口5000正在提供服务...');
