64561eea442844c587fe9881fc1704e231101ba6
sunxsh <sunxsaljj@163.com>
2016-10-11 10:49:53 +0800

    abc

---
+++ dotnet/JQueryPlugins/JQueryPlugins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JQueryPlugins.Startup))]
+namespace JQueryPlugins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
